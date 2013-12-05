b6830057068c2d13bd919dc4d5816c46f564d723
Brian Nakayama <briannkym@gmail.com>
2013-12-04 20:48:22 -0600

    integrated file

--- Breakout/Kbd.vhd
+++ Breakout/Kbd.vhd
@@ -1,81 +1,136 @@
 
-LIBRARY IEEE;
-USE  IEEE.STD_LOGIC_1164.all;
-USE IEEE.STD_LOGIC_ARITH.all;
-USE  IEEE.STD_LOGIC_UNSIGNED.all;
+library ieee;
+use ieee.STD_LOGIC_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
-        
 entity kbd is
-  port( clock           : in std_logic; 
+  port( clock           : in std_logic;
         reset           : in std_logic;
         keyboard_clk    : in std_logic;
         keyboard_data   : in std_logic;
         scan_code	: out std_logic_vector(7 downto 0);
-        scan_ready	: out std_logic);
+        scan_ready	: out std_logic;
+        LEDs            : out std_logic_vector (2 downto 0));
 end kbd;
 
 architecture behavioral of kbd is
 
-type receiver_status is (wait_start, start, parity, ready);  -- Control & data flow for the receiver
-signal control_state : receiver_status;
-signal bit_index : unsigned;                -- counter for the shift register
-signal one_counter : unsigned;              -- counter for the erro checking
-signal sum : unsigned;                      -- parity checking
-signal scan_code_reg : std_logic_vector (7 downto 0);  -- Register that hold the
-                                                       -- byte sent
+type receiver_status is (wait_start, start, parity, check);  -- Control & data flow for the receiver
+signal next_state    : receiver_status;
+signal current_state : receiver_status;
+
+signal bit_index     : integer range 0 to 8;          -- counter for the shift register
+signal one_counter   : integer ;                      -- counter for the erro checking
+signal sum           : integer;                       -- parity checking
+signal scan_code_reg : std_logic_vector (7 downto 0); -- Register that hold the
+                                                      -- byte sent
+signal scan_flag      : std_logic;
+signal scan_ready_reg : std_logic;
+signal scan_counter   : integer;
+
+signal main_counter : std_logic_vector(2 downto 0);
+
+signal LEDs_reg : std_logic_vector(2 downto 0);
+
 begin                                   --mixed
 
-  -- purpose: FSM for receiving bytes from keyboard
+
+
+  FSM: process (clock, reset,next_state, scan_flag)
+  begin  -- process FSM
+    if reset='0' then
+      scan_ready_reg    <= '0';
+      current_state <= wait_start;
+      scan_counter <= 0;
+    elsif(falling_edge(clock)) then
+      current_state <= next_state;
+      if scan_flag = '0'  then
+        scan_counter <= 0;
+      else
+        scan_counter <= scan_counter + 1;
+      end if;
+      if scan_counter = 4 then
+        scan_ready_reg <= '1';
+      else
+        scan_ready_reg <= '0';
+      end if;
+    end if;
+  end process FSM;
+
+-- purpose: FSM for receiving bytes from keyboard
   -- type   : combinational
   -- inputs : keyboard_clk
   -- outputs: 
-  control: process (keyboard_clk)
+  
+  control: process (keyboard_clk, reset)
   begin  -- process control
     if reset = '0' then
-      scan_ready    <= '0';
+      scan_flag <= '0';
       scan_code_reg <= (others => '0');
-      control_state <= wait_start;
       bit_index     <= 0;
       one_counter   <= 0;
       sum           <= 0;
-    elsif(falling_edge(keyboard_clk)) then
-      case (control_state) is
-        when  wait_start =>
-
-          if (keyboard_data = '0') then
-            control_state <= start;
-          end if;
-        when start =>
- 
-          if (keyboard_data = '1') then
-            one_counter <= one_counter + 1;
-          end if;
-
-          if (index = 7) then
-            index <= 0;
-            control_state <= ready;
-          else  
-            index <= index + 1;
-          end if;
-          scan_code_reg (6 downto 0) <= scan_code_reg (7 downto 1);
-          scan_code_reg (7)           <= keyboard_data;
-
-        when parity =>
-          sum           <= one_counter + keyboard_data;
-          one_counter   <= 0;
-          control_state <= ready;
-        when ready =>
-           if (sum mod 2) = 0 then
-             scan_code_reg <= (others => '0');
-             control_state <= wait_start;
-           else
-             scan_ready <= '1';
-           end if;
-          
-        when others => null;
-      end case;    
-
+      LEDs_reg      <= "111";
+      main_counter  <= "000";
+    else
+      
+      if(falling_edge(keyboard_clk)) then
+        one_counter <= one_counter;
+        case (current_state) is
+          when  wait_start =>             --detect the start bit
+            scan_flag <= '0';
+            if (keyboard_data = '0') then
+            
+              next_state <= start;
+            end if;
+          when start =>                   --sampling data
+           
+            if (keyboard_data = '1') then
+              one_counter <= one_counter + 1;
+            end if;
+            
+            if (bit_index = 7) then
+              LEDs_reg (0) <= not LEDs_reg(0);
+         
+              bit_index <= 0;
+              next_state <= parity;
+            else  
+              bit_index <= bit_index + 1;
+            end if;
+            scan_code_reg (6 downto 0) <= scan_code_reg (7 downto 1);
+            scan_code_reg (7)           <= keyboard_data;
+            
+          when parity =>
+            LEDs_reg (1) <= not LEDs_reg(1); 
+            if (keyboard_data = '1') then
+              sum           <= one_counter + 1;
+            else
+              sum           <= one_counter;
+            end if;	
+            one_counter   <= 0;
+            next_state <= check;
+        
+          when check =>
+            LEDs_reg (2) <= not LEDs_reg(2); 
+            if ((sum mod 2) = 1) and keyboard_data = '1' then
+              scan_flag <= '1';
+            end if;
+            next_state <= wait_start;
+            if scan_code_reg = x"F0" then
+              main_counter <= main_counter + 1;
+            end if;
+           
+          when others => null;
+            
+        end case;    
+        
+      end if;
     end if;
   end process control;
-  scan_code <= scan_code_reg;
+  scan_ready <= scan_ready_reg;             
+  scan_code  <= scan_code_reg;
+ -- LEDs       <= LEDs_reg;
+  LEDs       <= main_counter;
 end behavioral;
--- Breakout/Top_module.vhd
+++ Breakout/Top_module.vhd
@@ -1,131 +1,197 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    12:00:37 11/18/2013 
--- Design Name: 
--- Module Name:    Top_module - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+library ieee;
+use ieee.STD_LOGIC_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+library work;
+use work.breakout_config.all;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+entity top_module is
+  port (
+    clk   : in std_logic;
 
-entity Top_module is
-Port(
---The signals from the keyboard.
---PS2C: in std_logic;
---PS2D: in std_logic;
-
+--Keyboard signals
+    PS2C  : in std_logic;
+    PS2D  : in std_logic;
+    LEDs  : out std_logic_vector(7 downto 0);
+   
 --The signals to the VGA
-Hsync: out std_logic;
-Vsync: out std_logic;
-vgaRed: out std_logic_vector(3 downto 1);
-vgaGreen: out std_logic_vector(3 downto 1);
-vgaBlue: out std_logic_vector(3 downto 2);
+    Hsync: out std_logic;
+    Vsync: out std_logic;
+    vgaRed: out std_logic_vector(3 downto 1);
+    vgaGreen: out std_logic_vector(3 downto 1);
+    vgaBlue: out std_logic_vector(3 downto 2));
 
---Signals to the seven segment displays.
---seg: out std_logic_vector(6 downto 0);
---dp: out std_logic;
---an: out std_logic_vector(3 downto 0);
 
---The clock and reset signals.
-clk: in std_logic
-);
-end Top_module;
-architecture structural of Top_module is
+end top_module;
 
-Component VGA is
-Port(
+architecture top_level of top_module is
+
+  component kbd
+    port (
+      clock           : in std_logic; 
+      reset           : in std_logic;
+      keyboard_clk    : in std_logic;
+      keyboard_data   : in std_logic;
+      scan_code	      : out std_logic_vector(7 downto 0);
+      scan_ready      : out std_logic;
+      LEDs            : out std_logic_vector(2 downto 0));
+    
+  end component;
+
+  component controller
+    port(
+      clock           : in std_logic; 
+      reset           : in std_logic;
+      scan_ready      : in std_logic;
+      scan_code       : in std_logic_vector(7 downto 0);
+      LEDs            : out std_logic_vector(2 downto 0);
+      control_en      : out std_logic;
+      control_signal  : out control_signal_out);
+    
+  end component;
+
+  component game_logic 
+    port(
+      clock           : in std_logic; 
+      reset           : in std_logic;
+      control_en      : in std_logic;
+      control_signal	: in control_signal_out;
+        
+--Things specific to the game
+      paddle_x        : out std_logic_vector(11 downto 0); 
+      ball_x          : out std_logic_vector(11 downto 0);  
+      ball_y          : out std_logic_vector(11 downto 0);
+      bricks          : out std_logic_vector(127 downto 0); 
+      draw_mode       : out std_logic_vector(3 downto 0));
+
+  end component;
+
+  
+  Component VGA is
+    Port(
 --The Clock
-clk: in std_logic; 
-rst: in std_logic;
+      clk       : in std_logic; 
+      rst       : in std_logic;
 
 --The signals to the VGA
-Hsync: out std_logic;
-Vsync: out std_logic;
-vgaRed: out std_logic_vector(3 downto 1);
-vgaGreen: out std_logic_vector(3 downto 1);
-vgaBlue: out std_logic_vector(3 downto 2);
+      Hsync     : out std_logic;
+      Vsync     : out std_logic;
+      vgaRed    : out std_logic_vector(3 downto 1);
+      vgaGreen  : out std_logic_vector(3 downto 1);
+      vgaBlue   : out std_logic_vector(3 downto 2);
 
 
 --Things specific to the game
-paddle_x : in std_logic_vector(11 downto 0); 
-ball_x : in std_logic_vector(11 downto 0);  
-ball_y : in std_logic_vector(11 downto 0);
-bricks : in std_logic_vector(127 downto 0); 
-draw_mode : in std_logic_vector(3 downto 0)
-);
-End Component VGA;
+      paddle_x  : in std_logic_vector(11 downto 0); 
+      ball_x    : in std_logic_vector(11 downto 0);  
+      ball_y    : in std_logic_vector(11 downto 0);
+      bricks    : in std_logic_vector(127 downto 0); 
+      draw_mode : in std_logic_vector(3 downto 0)
+      );
+  End Component VGA;
 
 
-Component DCM
-Port(
-	CLKIN_IN : IN std_logic;
-	RST_IN : IN std_logic;          
-	CLKDV_OUT : OUT std_logic;
-	CLK0_OUT : OUT std_logic;
-	LOCKED_OUT : OUT std_logic
-	);
-end Component;
+  Component DCM
+    Port(
+      CLKIN_IN   : IN std_logic;
+      RST_IN     : IN std_logic;          
+      CLKDV_OUT  : OUT std_logic;
+      CLK0_OUT   : OUT std_logic;
+      LOCKED_OUT : OUT std_logic);
+  end Component;
+
+  --signals declaration
+ 
+ signal reset           : std_logic;
+--Keyboard signals
+ signal scan_ready      : std_logic;
+ signal scan_code       : std_logic_vector(7 downto 0);
+
+--Controller signals
+ signal control_en      : std_logic;
+ signal control_signal  : control_signal_out;
+--BreakRaster signals
+ signal paddle_x        : std_logic_vector(11 downto 0); 
+ signal ball_x          : std_logic_vector(11 downto 0);  
+ signal ball_y          : std_logic_vector(11 downto 0);
+ signal bricks          : std_logic_vector(127 downto 0); 
+ signal draw_mode       : std_logic_vector(3 downto 0);
+
+ signal lock            : std_logic;
+ signal clk25mhz        : std_logic;
+ signal clk50mhz        : std_logic;
+ signal clkdv           : std_logic;
+  
+
+begin  -- top_level
+ reset    <= '0', '1' after 25 ns, '0' after 75 ns;
+
+ keyboard : kbd port map (
+    clock         => clk,
+    reset         => not reset, 
+    keyboard_clk  => PS2C,
+    keyboard_data => PS2D,
+    scan_code     => scan_code,
+    scan_ready    => scan_ready,
+    LEDs          => LEDs (2 downto 0));
 
 
---Interconnect wires
-signal lock, clk25mhz, clk50mhz, rst, clkdv: std_logic;
+   control : Controller port map (
+    clock         => clk,
+    reset         => not reset, 
+    scan_ready    => scan_ready, 
+    scan_code     => scan_code,
+    LEDs(0)       => LEDs (3),
+    LEDs(1)       => LEDs (4),
+    LEDs(2)       => LEDs (6),
+    control_en    => control_en,
+    control_signal=> control_signal);
 
---Begin the structural architecture.
-begin
---The 25mhz clock is only working properly when lock is 1. 
+ game_log : game_logic port map (
+    clock          => clk,
+    reset          => not reset,
+    control_en     => control_en,
+    control_signal => control_signal,
+        
+--Things specific to the game
+    paddle_x       => paddle_x,
+    ball_x         => ball_x,
+    ball_y         => ball_y,
+    bricks         => bricks,
+    draw_mode      => draw_mode);
+ 
+ --The 25mhz clock is only working properly when lock is 1. 
 clk25mhz <= clkdv when lock='1' else '0';
-rst <= '0';
 
-VGA_impl: VGA
-port map(
+ VGA_impl: VGA port map(
 --The Clock
-clk => clk25mhz, 
-rst => rst,
+   clk       => clk25mhz, 
+   rst       => reset,
 
 --The signals to the VGA
-Hsync => Hsync,
-Vsync => Vsync,
-vgaRed => vgaRed,
-vgaGreen => vgaGreen,
-vgaBlue => vgaBlue,
+   Hsync     => Hsync,
+   Vsync     => Vsync,
+   vgaRed    => vgaRed,
+   vgaGreen  => vgaGreen,
+   vgaBlue   => vgaBlue,
 
 --Things specific to the game
-paddle_x => "000000111111", 
-ball_x => "000000111111", 
-ball_y => "000011111111",
-bricks => x"FFFFFFFFEEEEEEEECCCCCCCC11111111",
-draw_mode => (others => '0')
-);
+   paddle_x  => paddle_x,
+   ball_x    => ball_x,
+   ball_y    => ball_y,
+   bricks    => bricks,
+   draw_mode => draw_mode);
 
 
-Inst_DCM: DCM PORT MAP(
-		CLKIN_IN => clk,
-		RST_IN => rst,
-		CLKDV_OUT => clkdv ,
-		CLK0_OUT => clk50mhz,
-		LOCKED_OUT => lock
-	);
-
-end structural;
+ Inst_DCM: DCM PORT MAP(
+   CLKIN_IN   => clk,
+   RST_IN     => reset,
+   CLKDV_OUT  => clkdv ,
+   CLK0_OUT   => clk50mhz,
+   LOCKED_OUT => lock);
+ 
+end top_level;
+  
 
