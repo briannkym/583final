c2ecc7c9e75bf82fa1423bd3636be700116b7768
Brian Nakayama <briannkym@gmail.com>
2013-12-11 22:08:44 -0600

    Continued improvements to BreakRaster

--- Breakout/src/Character.vhd
+++ Breakout/src/Character.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -23,244 +24,244 @@ end Char;
 
 architecture dataflow of Char is
 
-constant a: std_logic_vector(63 downto 0):= 
+constant leta: std_logic_vector(63 downto 0):= 
 "0001100000111100001001000110011001111110010000101100001111000011";
 
-constant b: std_logic_vector(63 downto 0):= 
+constant letb: std_logic_vector(63 downto 0):= 
 "1111110011000110110000111111110011001110110000111100001111111110";
 
-constant c: std_logic_vector(63 downto 0):= 
+constant letc: std_logic_vector(63 downto 0):= 
 "0001111001110011110000001100000011000000110000000111001100011110";
 
-constant d: std_logic_vector(63 downto 0):= 
+constant letd: std_logic_vector(63 downto 0):= 
 "1111110011001110110000111100001111000011110000111100111011111100";
 
-constant e: std_logic_vector(63 downto 0):= 
+constant lete: std_logic_vector(63 downto 0):= 
 "1111111111000001110000001111000011111000110000001100000111111111";
 
-constant f: std_logic_vector(63 downto 0):= 
+constant letf: std_logic_vector(63 downto 0):= 
 "1111111111000001110000001111001011111110110000001100000011100000";
 
-constant g: std_logic_vector(63 downto 0):= 
+constant letg: std_logic_vector(63 downto 0):= 
 "0011110011100111110000001100000011000111110000111110011100111100";
 
-constant h: std_logic_vector(63 downto 0):= 
+constant leth: std_logic_vector(63 downto 0):= 
 "1110011111000011110000111110011111111111110000111100001111100111";
 
-constant i: std_logic_vector(63 downto 0):= 
+constant leti: std_logic_vector(63 downto 0):= 
 "1111111110011001000110000001100000011000000110001001100111111111";
 
-constant j: std_logic_vector(63 downto 0):= 
+constant letj: std_logic_vector(63 downto 0):= 
 "1111111100001100000011000000110011101100110011001100110001111000";
 
-constant k: std_logic_vector(63 downto 0):= 
+constant letk: std_logic_vector(63 downto 0):= 
 "1110001111000110110111001111000011011000110011001100011011100011";
 
-constant l: std_logic_vector(63 downto 0):= 
+constant letl: std_logic_vector(63 downto 0):= 
 "1110000011000000110000001100000011000000110000001100000111111111";
 
-constant m: std_logic_vector(63 downto 0):= 
+constant letm: std_logic_vector(63 downto 0):= 
 "1100001111000011111001111111111111011011110000111100001111000011";
 
-constant n: std_logic_vector(63 downto 0):= 
+constant letn: std_logic_vector(63 downto 0):= 
 "1100011111100011111100111101101111001111110001111100001111100111";
 
-constant o: std_logic_vector(63 downto 0):= 
+constant leto: std_logic_vector(63 downto 0):= 
 "0011110011100111110000111100001111000011110000111110011100111100";
 
 
-constant p: std_logic_vector(63 downto 0):= 
+constant letp: std_logic_vector(63 downto 0):= 
 "1111110011000111110000111100011111111110110000001100000011100000";
 
-constant q: std_logic_vector(63 downto 0):= 
+constant letq: std_logic_vector(63 downto 0):= 
 "0001100000111100001001000110011001111110010000101100001111000011";
 
-constant r: std_logic_vector(63 downto 0):= 
+constant letr: std_logic_vector(63 downto 0):= 
 "1111110011000111110000111100011011111100110011001100011011100111";
 
-constant s: std_logic_vector(63 downto 0):= 
+constant lets: std_logic_vector(63 downto 0):= 
 "0011111111100001110000000111000000111100000011101100011101111110";
 
-constant t: std_logic_vector(63 downto 0):= 
+constant lett: std_logic_vector(63 downto 0):= 
 "1111111110011001000110000001100000011000000110000001100000111100";
 
-constant u: std_logic_vector(63 downto 0):= 
+constant letu: std_logic_vector(63 downto 0):= 
 "1110011111000011110000111100001111000011110000111110011100111100";
 
-constant v: std_logic_vector(63 downto 0):= 
+constant letv: std_logic_vector(63 downto 0):= 
 "1110011111000011111001110110011001100110001001000011110000011000";
 
-constant w: std_logic_vector(63 downto 0):= 
+constant letw: std_logic_vector(63 downto 0):= 
 "0001100000111100001001000110011001111110010000101100001111000011";
 
-constant x: std_logic_vector(63 downto 0):= 
+constant letx: std_logic_vector(63 downto 0):= 
 "0001100000111100001001000110011001111110010000101100001111000011";
 
-constant y: std_logic_vector(63 downto 0):= 
+constant lety: std_logic_vector(63 downto 0):= 
 "1100001101100110001001000011110000011000000110000001100000111100";
 
-constant z: std_logic_vector(63 downto 0):= 
+constant letz: std_logic_vector(63 downto 0):= 
 "0001100000111100001001000110011001111110010000101100001111000011";
 
-signal pixel: std_logic :=0;
+signal pixel: std_logic :='0';
 
 begin 
 
-process(x, y, number)
+process(x, y, charnum)
 begin
-	case number is
+	case charnum is
 		when x"00" =>
-			if(a(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(leta(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"01" =>
-			if(b(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letb(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"02" =>
-			if(c(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letc(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"03" =>
-			if(d(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letd(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"04" =>
-			if(e(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(lete(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"05" =>
-			if(f(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letf(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"06" =>
-			if(g(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letg(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"07" =>
-			if(h(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(leth(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"08" =>
-			if(i(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(leti(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"09" =>
-			if(j(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letj(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"0a" =>
-			if(k(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letk(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"0b" =>
-			if(l(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letl(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"0c" =>
-			if(m(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letm(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"0d" =>
-			if(n(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letn(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"0e" =>
-			if(o(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(leto(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"0f" =>
-			if(p(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letp(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"10" =>
-			if(q(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letq(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"11" =>
-			if(r(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letr(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"12" =>
-			if(s(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(lets(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"13" =>
-			if(t(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(lett(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"14" =>
-			if(u(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letu(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"15" =>
-			if(v(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letv(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"16" =>
-			if(w(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letw(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"17" =>
-			if(x(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letx(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"18" =>
-			if(y(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(lety(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
 			end if;
 		when x"19" =>
-			if(z(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
+			if(letz(to_integer(unsigned(y) * 8 + unsigned(x))) = '1') then
 				pixel<='1';
 			else
 				pixel<='0';
@@ -317,4 +318,4 @@ begin
 end process;
 
 
-end Behavioral;
+end dataflow;
